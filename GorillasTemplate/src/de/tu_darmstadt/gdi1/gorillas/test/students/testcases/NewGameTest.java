60173177d83aa722650ca86af04a76ea50715fcf
Crankicake <salim.karacaoglan@yahoo.de>
2015-02-10 17:05:38 +0100

    Hab den real Shit reingeladen
    Das ist der Urstatus von Gorilas

---
+++ GorillasTemplate/src/de/tu_darmstadt/gdi1/gorillas/test/students/testcases/NewGameTest.java
@@ -0,0 +1,37 @@
+package de.tu_darmstadt.gdi1.gorillas.test.students.testcases;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tu_darmstadt.gdi1.gorillas.test.adapter.GorillasTestAdapterMinimal;
+import de.tu_darmstadt.gdi1.gorillas.test.setup.TestGorillas;
+
+public class NewGameTest {
+
+	GorillasTestAdapterMinimal adapter;
+
+	@Before
+	public void setUp() {
+		adapter = new GorillasTestAdapterMinimal();
+	}
+
+	@After
+	public void finish() {
+		adapter.stopGame();
+	}
+	
+	@Test
+	public void testNewGame() {
+
+		// Testet, ob das Spiel aus dem MainMenuState, wenn "n" gedrückt wird,
+		// in den GameSetupState wechselt, wie es in der Aufgabenstellung unter
+		// "Neues Spiel starten" vorgegeben ist.
+		adapter.initializeGame();
+		assertTrue(adapter.getStateBasedGame().getCurrentStateID() == TestGorillas.MAINMENUSTATE);
+		adapter.handleKeyPressN();
+		assertTrue(adapter.getStateBasedGame().getCurrentStateID() == TestGorillas.GAMESETUPSTATE);
+	}
+}
