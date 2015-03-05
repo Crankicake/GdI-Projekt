9f7273915d9ac3dd1c1dd853d838cec317aeed83
Sonnexo <fabian.czappa@stud.tu-darmstadt.de>
2015-03-06 00:28:38 +0100

    Simon wars

---
+++ GorillasTemplate/src/TestFürSimon.java
@@ -0,0 +1,12 @@
+
+public class TestFürSimon {
+
+	public TestFürSimon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hallo");
+	}
+	
+}
