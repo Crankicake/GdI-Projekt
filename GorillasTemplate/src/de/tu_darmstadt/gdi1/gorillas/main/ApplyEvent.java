e5181e8b9e6a99aa6bd77c2de7a6c32176e97c4a
Sonnexo <fabian.czappa@stud.tu-darmstadt.de>
2015-03-09 20:43:36 +0100

    Keine ID mehr

--- GorillasTemplate/src/de/tu_darmstadt/gdi1/gorillas/main/ApplyEvent.java
+++ GorillasTemplate/src/de/tu_darmstadt/gdi1/gorillas/main/ApplyEvent.java
@@ -9,15 +9,8 @@ public class ApplyEvent extends Event {
 
 	private static boolean performActionBoolean;
 	
-	@SuppressWarnings("unused")
-	private double id;
-	
-	private long threadID = -1;
-	
 	public ApplyEvent() {
 		super("ButtonTest123");
-		
-		id = Math.random();
 		
 		performActionBoolean = false;
 	}
@@ -25,15 +18,10 @@ public class ApplyEvent extends Event {
 	@Override
 	protected boolean performAction(GameContainer gc, StateBasedGame sb,
 			int delta) {
-		if(Thread.currentThread().getId() != threadID && threadID != -1)
-		{
-			System.out.println("Fehler in den Threads");
-		}
 		return performActionBoolean;
 	}
 
 	public void SetPerformAction(boolean b) {
 		performActionBoolean = b;
-		threadID = (Thread.currentThread().getId());
 	}
 }
